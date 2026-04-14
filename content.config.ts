1e7a394a28d0749dd5d8bdd2120e5bbc5282b071
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-14 15:15:09 +1200

    Added Zod for typing.

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,18 @@
 import { defineContentConfig, defineCollection } from "@nuxt/content";
+import { z } from "zod";
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    blog: defineCollection({
       type: "page",
       source: "**/*.md",
+      schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
+        image: z.string(),
+        date: z.coerce.date(),
+      }),
     }),
   },
 });
