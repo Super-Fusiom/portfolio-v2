ad4f75d73c465803a0c3004f9b2b81c83e443180
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-14 15:16:44 +1200

    Removed md renderer on home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-const { data: home } = await useAsyncData(() =>
-    queryCollection("content").path("/").first(),
-);
-
-useSeoMeta({
-    title: home.value?.title,
-    description: home.value?.description,
-});
-</script>
-
 <template>
-    <ContentRenderer v-if="home" :value="home" />
-    <div v-else>Home not found</div>
+    <section class="hero">
+        <h1>Hi, I'm Paul</h1>
+        <p>I build and self-host secure systems.</p>
+    </section>
 </template>
