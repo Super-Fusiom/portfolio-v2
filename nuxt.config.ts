313e986ea07c10df0c62a50f098a699c03bf8f73
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-14 15:16:10 +1200

    SEO, using new modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Paul@SF",
+      titleTemplate: "%s . Paul@SF",
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+  site: {
+    name: "Paul@SF",
+    url: "https://superfusiom.net",
+  },
+  seo: {
+    meta: {
+      titleTemplate: "%s . %siteName",
+    },
+  },
   vite: {
     optimizeDeps: {
       include: [],
     },
   },
   ogImage: { enabled: false },
-  ssr: false,
+  ssr: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/content", "@nuxt/image", "@nuxtjs/seo", "@nuxt/icon"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/image",
+    "@nuxtjs/seo",
+    "@nuxt/icon",
+    "@vueuse/nuxt",
+  ],
 });
