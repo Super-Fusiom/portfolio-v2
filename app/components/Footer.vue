1f938f8a75b0d9311e419686f96d3dc27be8bf71
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-16 20:31:18 +1200

    Added icons to footer for social links.

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
     <footer>
         <p>© 2026 Paul Singh</p>
+        <div>
+            <Icon name="uil:github" />
+            <Icon name="uil:linkedin" />
+        </div>
     </footer>
 </template>
