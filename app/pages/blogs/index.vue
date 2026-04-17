444f1c5939646a13efda5c5a0b5508131ada68fa
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-17 20:02:03 +1200

    Added details to the Blogs page

--- app/pages/blogs/index.vue
+++ app/pages/blogs/index.vue
@@ -10,6 +10,14 @@ const { data: posts } = await useAsyncData("blog", () => {
 <template>
     <div>
         <h1>Blogs</h1>
+        <p>
+            Here are the things I've been up to, project details fall under
+            this.
+        </p>
+        <p>
+            If a project is updated, then the blog talking about it will also be
+            updated as well as what I've changed.
+        </p>
         <div class="list">
             <NuxtLink v-for="post in posts" :key="post.path" :to="post.path">
                 <div class="card">
