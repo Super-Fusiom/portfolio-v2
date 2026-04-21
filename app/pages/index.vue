e8a08fdab8d3b3b655668b9d667db2e84ab39041
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-04-21 18:49:33 +1200

    Adding extra text to hero.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <section class="hero">
         <h1>Hi, I'm Paul</h1>
-        <p>I build and self-host secure systems.</p>
+        <h3>I build and self-host secure systems.</h3>
+        <br />
+        <p>IT and Networking are my bread and butter.</p>
     </section>
 </template>
