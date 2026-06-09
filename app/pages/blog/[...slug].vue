1859ee2ac379a0667fae74c8f6698eb01f38b213
Paul Singh <80376466+Super-Fusiom@users.noreply.github.com>
2026-06-09 17:06:41 +1200

    Create [...slug].vue

---
+++ app/pages/blog/[...slug].vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const route = useRoute();
+const { data: post } = await useAsyncData(`blog-post-${route.params.id}`, () =>
+    queryCollection("blog").path(route.path).first(),
+);
+
+if (post.value) {
+    useSeoMeta({
+        title: () => post.value?.title,
+        ogTitle: () => post.value?.title,
+        description: () => post.value?.description,
+        ogDescription: () => post.value?.description,
+
+        author: () => post.value?.author,
+        articlePublishedTime: () => post.value?.date,
+    });
+}
+</script>
+
+<template>
+    <ContentRenderer class="md-content" v-if="post" :value="post" />
+    <h1 v-else>Post not found</h1>
+</template>
